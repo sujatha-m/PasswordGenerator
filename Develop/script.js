e76f1bc06895dca44c7f50603132d5267130b61c
sujatha <jhanavi.bhushan@gmail.com>
2020-02-28 21:52:15 -0500

    Code updated,formatted.ReadMe updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,105 +1,121 @@
-// Assignment Code
-
-/*let generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
-
 let generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(passwordLength,charType) {
+/* Function that generates random password and returns the password string
+ * Parameter[in]   passwordLength - holds the user specified length
+ * Parameter[in]   charType       - holds the character types selected by the user
+ */
+function generatePassword(passwordLength, charType) {
   let returnVal = "";
   for (let i = 0, n = charType.length; i < passwordLength; ++i) {
-      returnVal += charType.charAt(Math.floor(Math.random() * n));
+    // Generate random integers between 0 and n.Then the integers are used as index into the
+    // character types string chosen by the user
+    returnVal += charType.charAt(Math.floor(Math.random() * n));
   }
   return returnVal;
 }
 
-function writePassword(){
+/* Main function that accepts the following input arguments from user
+ * 1. Password Length as prompt
+ * 2. lower case character type as confirm
+ * 3. upper case character type as confirm
+ * 4. Numerals as confirm
+ * 5. Special character type as confirm
+ *
+ * Validates the input arguments and generates password to be displayed in the Box
+ */
+function writePassword() {
+  let text = ""; //variable that holds the text messages to be displayed to the user
+  let charNum = 0; //holds the number of character types chosen by the user
+  let charType = ""; //holds the input character type selected by the user
+  let flag = true; // flag to indicate if we are good to proceed with password generation
+  /* default string to be displayed if input validation failed.
+   * will be updated with generated password when all the criteria are matched
+   */
+  let GeneratedPassword = "No Password Generated";
 
-let text = "";
-let charNum = 0;
-let charType = "";
-let flag = true;
-let GeneratedPassword = "No Password Generated";
+  let passwordLength = prompt(
+    "Please enter length of password between 8 and 128 characters"
+  );
 
-let passwordLength = prompt ("Please enter length of password between 8 and 128 characters");
+  //if cancel was pressed or empty string given
+  if (passwordLength === null || passwordLength === "") {
+    //shows alert to the user
+    alert("passwordLength not given!! Cannot generate password");
+    flag = false; //clear the flag since no password length given
+  } else {
+    //OK pressed after entering a value
+    //check if password length is atleast 8 and no more than 128
+    if (Number(passwordLength) >= 8 && Number(passwordLength) <= 128) {
+      text = "Password of length " + passwordLength + " would be generated\n";
 
-if(passwordLength === null || passwordLength === ""){
-  alert("passwordLength not given!! Cannot generate password");
-  flag = false;
-} else{
-  if((Number(passwordLength) >= 8) && (Number(passwordLength) <= 128)){
-    text = "Password of length " + passwordLength + " would be generated";
+      let lowerCase = confirm(
+        "Include lowercase letters (abcdefghijklmnopqrstuvwxyz)?"
+      );
 
-    let lowerCase = confirm("Include lowercase letters (abcdefghijklmnopqrstuvwxyz)?");
-
-    if(lowerCase === null) {
-      text = text + "Lowercase letter not included";
-    } else {
-      charNum++;
-      charType = charType + "abcdefghijklmnopqrstuvwxyz";
-      text = text + "Lowercase letters included";
-    }
-
-    let upperCase = confirm("Include uppercase letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)?");
-
-    if(upperCase === null) {
-      text = text + "Uppercase letter not included";
-    } else {
-      charNum++;
-      charType = charType + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      text = text + "Uppercase letters included";
-    }
-
-    let numbers = confirm("Include numbers (0123456789)?");
-
-    if(numbers === null) {
-      text = text + "Numbers not included";
-    } else {
-      charNum++;
-      charType = charType + "0123456789";
-      text = text + "Numbers included";
-    }
-
-    let specialCharacters = confirm("Include special characters (!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)?");
-
-    if (specialCharacters === null) {
-      text = text + "Special Characters not included";
-
-    } else {
-      charNum++;
-      charType = charType + "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-      text = text + "Special Characters included";
-    }
-
-    if (charNum === 0){
-      alert ("No characters selected! Atleast one is required for password generation");
-      flag = false
-    }
-
-    if(flag === true) {
-      text = text + "\nProceed with Password Generation?\n"
-       if(confirm(text)) {
-          GeneratedPassword = generatePassword(passwordLength,charType);
+      if (lowerCase === false) {
+        text = text + "Lowercase letter not included\n";
+      } else {
+        charNum++;
+        charType = charType + "abcdefghijklmnopqrstuvwxyz";
+        text = text + "Lowercase letters included\n";
       }
-  }    
- } else {
-      alert("Invalid password length given!! Cannot generate password");
-  } 
-}
-  let passwordText = document.querySelector("#password");
-  passwordText.value = GeneratedPassword;
-}
 
+      let upperCase = confirm(
+        "Include uppercase letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)?"
+      );
+
+      if (upperCase === false) {
+        text = text + "Uppercase letter not included\n";
+      } else {
+        charNum++;
+        charType = charType + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        text = text + "Uppercase letters included\n";
+      }
+
+      let numbers = confirm("Include numbers (0123456789)?");
+
+      if (numbers === false) {
+        text = text + "Numbers not included\n";
+      } else {
+        charNum++;
+        charType = charType + "0123456789";
+        text = text + "Numbers included\n";
+      }
+
+      let specialCharacters = confirm(
+        "Include special characters (!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~)?"
+      );
+
+      if (specialCharacters === false) {
+        text = text + "Special Characters not included\n";
+      } else {
+        charNum++;
+        charType = charType + "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+        text = text + "Special Characters included\n";
+      }
+
+      if (charNum === 0) {
+        alert(
+          "No characters selected! Atleast one is required for password generation"
+        );
+        flag = false; //clear the flag since no character type was selected
+      }
+
+      if (flag === true) {
+        text = text + "\nProceed with Password Generation?\n";
+        // present final confirmation message to user before calling generatepassword function
+        if (confirm(text)) {
+          GeneratedPassword = generatePassword(passwordLength, charType);
+        }
+      }
+    } else {
+      //shows alert to the user
+      alert("Invalid password length given!! Cannot generate password");
+    }
+  }
+  let passwordText = document.querySelector("#password");
+  passwordText.value = GeneratedPassword; // Write password to the #password input
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # PasswordGenerator
+
+## Overview 
+An application that generates a random password based on the following user-selected criteria:-
+
+1. Password length between 8 and 128 characters
+2. Atleast 1 of the following character types selected
+   a. Lower case character type
+   b. Upper case character type
+   c. Numerals character type
+   d. Special character type
+
+This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It also features a clean,polished 
+and responsive user interface, ensuring that it adapts to multiple screen sizes.
+
+## Functionality
+The user clicks on "Generate Password" button to first launch a password length prompt.The password length input is validated and if the criteria is satisfied,each of the character type confirmations are then presented to the user.If atleast 1 character type is selected,then the password generation function is called and the generated password string with selected character types is displayed in the box.
+If any of the password generation criteria fails, an alert is displayed to the user with the reason for failure.
+
+The following images demonstrate the application functionality:
+
+![password generator demo](./Assets\1.PasswordLengthPrompt.png)
+![password generator demo](./Assets\2.lowercaseconfirmation.png)
+![password generator demo](./Assets\3.uppercaseconfirmation.png)
+![password generator demo](./Assets\4.Numeralsconfirmation.png)
+![password generator demo](./Assets\5.Specialcharactersconfirmation.png)
+![password generator demo](./Assets\6.Alloptionsconfirmation.png)
+![password generator demo](./Assets\7.PasswordGenerated.png)
+![password generator demo](./Assets\8.InvalidPasswordLengthAlert.png)
+![password generator demo](./Assets\9.NocharactertypesAlert.png)
